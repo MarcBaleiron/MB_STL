00ec867a608cc986810bb4bd3c43bdadc8935e6a
MarcBaleiron <mbaleand@myuax.com>
2024-12-03 10:43:40 +0100

    Ejercicio 1 mejorado

--- Ej_1/main.cpp
+++ Ej_1/main.cpp
@@ -6,4 +6,6 @@ int main ()
 {
     cout << "La suma de 3 y 8 es: " << Matematicas::suma (3, 8) << endl;
     cout << "La resta de 8 y 5 es: " << Matematicas::resta (8, 5) << endl;
+
+    return 0;
 }
